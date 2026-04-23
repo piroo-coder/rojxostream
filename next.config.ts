83d53852df790c5f9710d3132e74b1f6da50fb94
Saroj Kafle <kaflesaroj18@gmail.com>
2026-04-23 09:00:18 +0000

    increase its transparency such that it is barely visible (_for element <

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  devIndicators: false,
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
   images: {
     remotePatterns: [
       {
