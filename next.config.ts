09aecf684abad21a0cd122dee90a3a40d71e6545
Piro Coder <coderpiro@gmail.com>
2026-04-24 16:10:00 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve (<d

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['genkit', '@genkit-ai/google-genai', '@genkit-ai/next'],
   typescript: {
     ignoreBuildErrors: true,
   },
